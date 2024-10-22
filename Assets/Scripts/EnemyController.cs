edc71e78a93a07c4468f3d0a14468b66c02267c6
CrystalGmr <arfrenchhorn@gmail.com>
2024-10-22 08:08:22 -0500

    Fixed Enemy Movement
    Idk why I just had the enemy move on a timer last night lol. It's fixed now so the enemy only picks a random direction after the player finishes moving

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,11 +1,21 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Transform movePoint;
-    public double moveInterval;
-    private double timeLastChecked = 0.0;
+    public Transform playerMovePoint;
+    public Transform player;
+    private bool playerWasMoving = false;
+
+    private bool PlayerIsMoving
+    {
+        get
+        {
+            return Vector3.Distance(playerMovePoint.position, player.position) > 0.5f;
+        }
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,9 +28,9 @@ public class EnemyController : MonoBehaviour
     {
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
-        if (Vector3.Distance(transform.position, movePoint.position) == 0 && (Time.timeAsDouble - moveInterval > timeLastChecked))
+        // calculate and start next move
+        if (Vector3.Distance(transform.position, movePoint.position) == 0 && (playerWasMoving && !PlayerIsMoving))
         {
-            timeLastChecked = Time.timeAsDouble;
             int moveBy = Random.Range(-1, 2);
 
             if (Random.Range(0, 2) == 0)
@@ -32,5 +42,7 @@ public class EnemyController : MonoBehaviour
                 movePoint.position += new Vector3(0f, moveBy, 0f);
             }
         }
+
+        playerWasMoving = PlayerIsMoving;
     }
 }
